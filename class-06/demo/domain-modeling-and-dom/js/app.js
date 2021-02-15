69986bbc6551d5aee3e59d04cb1730e2d9a217e2
JB-Tellez <jb.tellez@gmail.com>
2021-02-15 11:00:44 -0800

    adds another demo step

--- class-06/demo/domain-modeling-and-dom/js/app.js
+++ class-06/demo/domain-modeling-and-dom/js/app.js
@@ -1,1 +1,23 @@
-console.log(document.getElementById('kittenProfiles'));
+let kittenContainerElem = document.getElementById('kittenProfiles');
+
+let articleElem = document.createElement('article');
+
+kittenContainerElem.appendChild(articleElem);
+
+let nameHeaderElem = document.createElement('h2');
+
+nameHeaderElem.textContent = 'Frankie';
+
+articleElem.appendChild(nameHeaderElem);
+
+/*
+
+<article>
+  <h2>.....</h2>
+</article>
+
+console.log(kittenContainerElem);
+*/
+
+
+
