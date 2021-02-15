d6daff308f1a0f4f3c16ed6840a57c49443a0301
JB-Tellez <jb.tellez@gmail.com>
2021-02-15 12:05:20 -0800

    completes demo

--- class-06/demo/domain-modeling-and-dom/js/app.js
+++ class-06/demo/domain-modeling-and-dom/js/app.js
@@ -1,23 +1,67 @@
+
+
+let frankie = {
+  name: 'Frankie',
+  age: 1,
+  interests: ['cuddling','chasing string','catnip'],
+};
+
+let jumper = {
+  name: 'Jumper',
+  age: 2,
+  interests: ['jumping','bounding','leaping'],
+};
+
+let serena = {
+  name: 'Serena',
+  age: 6,
+  interests: ['domination','grudging pets','being aloof'],
+};
+
 let kittenContainerElem = document.getElementById('kittenProfiles');
 
-let articleElem = document.createElement('article');
+function displayKitten(kittenObj) {
 
-kittenContainerElem.appendChild(articleElem);
+  let articleElem = document.createElement('article');
 
-let nameHeaderElem = document.createElement('h2');
+  kittenContainerElem.appendChild(articleElem);
 
-nameHeaderElem.textContent = 'Frankie';
+  let nameHeaderElem = document.createElement('h2');
 
-articleElem.appendChild(nameHeaderElem);
+  nameHeaderElem.textContent = kittenObj.name;
 
-/*
+  articleElem.appendChild(nameHeaderElem);
 
-<article>
-  <h2>.....</h2>
-</article>
+  // p tag with bio
+  let bioElem = document.createElement('p');
+  bioElem.textContent = kittenObj.name + ' is adorable and they are ' + kittenObj.age + ' years old';
+  articleElem.appendChild(bioElem);
 
-console.log(kittenContainerElem);
-*/
+  // interests
+  let interestsElem = document.createElement('ul');
+  articleElem.appendChild(interestsElem);
+
+  for(let i=0; i < kittenObj.interests.length; i++) {
+    let interestText = kittenObj.interests[i];
+    let li = document.createElement('li');
+    li.textContent = interestText;
+    interestsElem.appendChild(li);
+  }
+
+  // image
+  let imgElem = document.createElement('img');
+  articleElem.appendChild(imgElem);
+  imgElem.setAttribute('src', 'images/' + kittenObj.name.toLowerCase() + '.jpeg');
+  imgElem.setAttribute('alt', 'cute picture of ' + kittenObj.name + ', who is an orange and white cat. You should really adopt him.');
+
+}
+
+let kittens = [jumper, frankie, serena];
+
+for(let i=0; i < kittens.length; i++) {
+  let kitten = kittens[i];
+  displayKitten(kitten);
+}
 
 
 
